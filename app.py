1e6de249a6ee40de3447b30729936661136dccc3
KOJI <koji@example.com>
2026-05-25 07:23:56 +0900

    fix render path

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import sqlite3
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 DB_PATH = BASE_DIR / "data" / "saitama.db"
 
 app = Flask(
